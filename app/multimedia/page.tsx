db0207dcc90d282b983567522b97f4fd3f47946d
dev.sandakelum <dev.sandakelum@example.com>
2025-10-14 01:17:09 +0530

    Implement code changes to enhance functionality and improve performance

--- app/multimedia/page.tsx
+++ app/multimedia/page.tsx
@@ -1,8 +1,8 @@
-import MultimediaStudyGuide from '@/components/multimedia_study_guide'
+
 import React from 'react'
 
 export default function page() {
     return (
-        <MultimediaStudyGuide />
-  )
+        <></>
+    )
 }
