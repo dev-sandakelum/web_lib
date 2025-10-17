35d447dcbc44ac377baac961c739ac622aa33c93
dev.sandakelum <dev.sandakelum@example.com>
2025-10-18 01:58:05 +0530

    ggg

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: false,
+    dirs: ['app', 'components', 'resourses'],
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
 };
 
 export default nextConfig;
